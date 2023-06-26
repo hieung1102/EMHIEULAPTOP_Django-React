52ac6ef7b2c24ad89173e2bd71a4044d45ac4805
hieung1102 <hieung11022001@gmail.com>
2023-06-26 23:44:41 +0700

    update app

--- server/app/views.py
+++ server/app/views.py
@@ -4,16 +4,16 @@ from django.template import loader
 from .models import SANPHAM
 # Create your views here.
 def HOME(request):
-    templates = loader.get_template('home.html')
+    templates = loader.get_template('app/home.html')
     return HttpResponse(templates.render())
 
 
 def Login(request):
-    templates = loader.get_template('login.html')
+    templates = loader.get_template('app/login.html')
     return HttpResponse(templates.render())
 
 def Register(request):
-    templates = loader.get_template('register.html')
+    templates = loader.get_template('app/register.html')
     return HttpResponse(templates.render())
 
 def ShowProduct(request):
@@ -21,7 +21,7 @@ def ShowProduct(request):
     sp = SANPHAM.objects.all().values()
     # filter 
     # sp = SANPHAM.objects.filter(TenSP__startswith = 'A').values
-    template = loader.get_template('product.html')
+    template = loader.get_template('app/product.html')
     context = {
         'sp':sp,
     }
